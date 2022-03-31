b4365571048479213e2ac2f9931a31f415f09efb
Ignite <ignite21@protonmail.com>
2022-04-01 01:11:27 +0200

    update

--- resources/js/src/pages/Signup/index.js
+++ resources/js/src/pages/Signup/index.js
@@ -1,5 +1,5 @@
 // React
-import React from "react";
+import React, { useState } from "react";
 import { Inertia } from "@inertiajs/inertia";
 import { useForm } from "@inertiajs/inertia-react";
 
@@ -8,10 +8,14 @@ import Layout from "@components/Layout";
 import Input from "@components/Form/Input";
 import Button from "@components/Button";
 
+// Recaptcha
+import Recaptcha from "react-recaptcha";
+
 // Styles
 import "@assets/styles/form.css";
 
 function Signup() {
+    const [recaptcha, setRecaptcha] = useState(false);
     const { data, setData, post, processing, cancel, errors, reset } = useForm({
         first_name: "",
         last_name: "",
@@ -24,10 +28,12 @@ function Signup() {
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        post(route("signup.store"), {
-            onSuccess: () => reset(),
-            onError: () => cancel(),
-        });
+        if (recaptcha) {
+            post(route("signup.store"), {
+                onSuccess: () => reset(),
+                onError: () => cancel(),
+            });
+        }
     };
 
     return (
@@ -85,6 +91,15 @@ function Signup() {
                 error={errors.password_confirmation}
                 required
             />
+            <Recaptcha
+                sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
+                render="explicit"
+                onloadCallback={() =>
+                    console.log("Recaptcha successfully loaded.")
+                }
+                verifyCallback={(response) => response && setRecaptcha(true)}
+            />
+            {!recaptcha && <p className="input-error">Recaptcha required.</p>}
             <Button type="submit" name="CREATE ACCOUNT" disabled={processing} />
             <div className="form__footer">
                 <p className="form__footer-text">Already have an account?</p>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
     <script src="{{ mix('/js/app.js') }}" defer></script>
+    <script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit" async defer></script>
     @routes
 </head>
 
